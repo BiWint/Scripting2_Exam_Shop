7ba723b6e59d7f703e1ce73a8b70f1b4c4b7efa5
Birgitte <birgitte.wintersto@outlook.com>
2020-05-27 00:55:10 +0200

    Player now changes skins + juice
    We can now choose which "skin" or material the player can have.
    Added background and song  for some juice

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -12,14 +12,26 @@ public class mainMenu : MonoBehaviour
     public GameObject shopButtonPrefab;
     public GameObject shopButtonContainer;
 
+    [Header("Material for player")]
+    public Material Player;
+
     private void Start()
     {
+
+        ChangePlayerSkin(0);
+
+        int textureIndex = 0;
+
         Sprite[] textures = Resources.LoadAll<Sprite>("Player");
         foreach(Sprite texture in textures)
         {
             GameObject container = Instantiate(shopButtonPrefab) as GameObject;
             container.GetComponent<Image>().sprite = texture;
             container.transform.SetParent(shopButtonContainer.transform, false);
+
+            int index = textureIndex;
+            container.GetComponent<Button>().onClick.AddListener(()=>ChangePlayerSkin(index));
+            textureIndex++;
         }
     }
 
@@ -28,4 +40,24 @@ public class mainMenu : MonoBehaviour
         Camera.main.transform.LookAt(menuTransform.position);
 
     }
+
+    public void ChangePlayerSkin(int index)
+    {
+        float x = (index % 4) * 0.25f;
+        float y = (index / 4) * 0.25f;
+
+        //Making sure Unity is reading the materials in correct order from top left to bottom right on the material
+        if (y == 0.0f)
+            y = 0.75f;
+        else if (y == 0.25f)
+            y = 0.5f;
+        else if (y == 0.50f)
+            y = 0.25f;
+        else if (y == 0.75f)
+            y = 0;
+
+        Player.SetTextureOffset("_MainTex", new Vector2(x, y));
+
+    }
+
 }
